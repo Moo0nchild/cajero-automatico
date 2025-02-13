a1da0323ce993d007e0a3148dc796b315bdfca8c
mibichoandafugao <augustohenao1234@gmail.com>
2025-02-12 21:45:54 -0500

    Files Added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hola, bienvenido a mi cajero</h1>
+      <p>Bienvenido a Cajero</p>
+      <p>
+        No me acuerdo muy bien de los shortcuts y de como programar en react
+        pero intentare dar lo mejor de mi
+      </p>
+    </>
+  )
+}
+
+export default App
